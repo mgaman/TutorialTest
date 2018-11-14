1813d47d5eab7b781eade4952b56a834c255628e
David Henry <mgadriver@gmail.com>
2018-11-14 22:06:00 +0200

    Added printf

--- app1/main.c
+++ app1/main.c
@@ -3,7 +3,11 @@
 /**
  * main.c
  */
+
+#include <stdio.h>
+#include "names.h"
 int main(void)
 {
+    printf ("Welcome to my home %d",David);
 	return 0;
 }
---
+++ app1/names.h
@@ -0,0 +1,13 @@
+/*
+ * names.h
+ *
+ *  Created on: Nov 14, 2018
+ *      Author: susan
+ */
+
+#ifndef NAMES_H_
+#define NAMES_H_
+
+const char* David = "David";
+
+#endif /* NAMES_H_ */
