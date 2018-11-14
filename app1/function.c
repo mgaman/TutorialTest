4805dccfe8b6c6d84bcf8d8715b9548c7676ffbd
David Henry <mgadriver@gmail.com>
2018-11-14 22:32:30 +0200

    Add external function

---
+++ app1/function.c
@@ -0,0 +1,13 @@
+/*
+ * function.c
+ *
+ *  Created on: Nov 14, 2018
+ *      Author: susan
+ */
+
+int function(int n)
+{
+    return n*n;
+}
+
+
--- app1/main.c
+++ app1/main.c
@@ -4,8 +4,12 @@
 
 #include <stdio.h>
 #include "names.h"
+
+// prototypes
+int function(int n);
+
 int main(void)
 {
-    printf ("Welcome to my home %d",allNames[0]);
+    printf ("Welcome to my home %d, weight %d",allNames[0],function(3));
 	return 0;
 }
